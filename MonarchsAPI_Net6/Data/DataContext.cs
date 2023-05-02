4c2e1dffd3f1b53ea85fe839f5fee67e543fc551
JeremyM45 <jeremyvs1945@gmail.com>
2023-05-01 20:09:13 -0700

    add DataContext

---
+++ MonarchsAPI_Net6/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MonarchsAPI_Net6.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+              
+        }
+
+    }
+}
