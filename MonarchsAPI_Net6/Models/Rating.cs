17309c95cefeda5fb260f98595644a9990707815
JeremyM45 <jeremyvs1945@gmail.com>
2023-05-01 20:05:16 -0700

    add Rating model

---
+++ MonarchsAPI_Net6/Models/Rating.cs
@@ -0,0 +1,9 @@
+﻿namespace MonarchsAPI_Net6.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public float RatingValue { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
