df80e986d22bbdbe9eddc8c87b11d359a790ba79
JeremyM45 <jeremyvs1945@gmail.com>
2023-05-01 21:10:21 -0700

    add UserController

---
+++ MonarchsAPI_Net6/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MonarchsAPI_Net6.Data;
+using MonarchsAPI_Net6.Models;
+using MonarchsAPI_Net6.Services.UserServices;
+
+namespace MonarchsAPI_Net6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices _userServices;
+        public UserController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAll()
+        {
+            List<User> users = await _userServices.GetAllUsers();
+            if(users == null) { return NotFound(); }
+            return Ok(users);
+        }
+    }
+}
