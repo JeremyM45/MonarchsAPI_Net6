679f247f7312681e113a68e72ee266dbb4894158
JeremyM45 <jeremyvs1945@gmail.com>
2023-05-02 17:51:59 -0700

    add CountryController

---
+++ MonarchsAPI_Net6/Controllers/CountryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MonarchsAPI_Net6.Services.CountryServices;
+
+namespace MonarchsAPI_Net6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly ICountryServices _countryServices;
+        public CountryController(ICountryServices countryServices)
+        {
+            _countryServices = countryServices;
+        }
+    }
+}
