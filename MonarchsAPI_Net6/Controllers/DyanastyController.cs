642189cc8c897e18251f76c52d81f7530d352784
JeremyM45 <jeremyvs1945@gmail.com>
2023-05-02 17:46:40 -0700

    add DynastyController

---
+++ MonarchsAPI_Net6/Controllers/DyanastyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MonarchsAPI_Net6.Services.DynastyServices;
+
+namespace MonarchsAPI_Net6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DyanastyController : ControllerBase
+    {
+        private readonly IDynastyServices _dynastyServices;
+        public DyanastyController(IDynastyServices dynastyServices)
+        {
+            _dynastyServices = dynastyServices;
+        }
+
+
+    }
+}
