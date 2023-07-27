aa6ad1780326a9ce319fd93c7fc037b3bb07ceb1
JeremyM45 <jeremyvs1945@gmail.com>
2023-07-26 19:12:03 -0500

    add files to publish to Azure

--- MonarchsAPI_Net6/Program.cs
+++ MonarchsAPI_Net6/Program.cs
@@ -59,11 +59,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Monarchs API");
+    c.RoutePrefix = string.Empty;
+});
+
+
 
 app.UseHttpsRedirection();
 
